22ae21c20c688d1d150faf17b86273ebaa328b59
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2022-12-23 15:36:55 +0100

    final

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -12,7 +12,21 @@ const GlobalStyles = createGlobalStyle`
     body {
         font-family: Christmas, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
         max-width: 800px;
-        background-color: black;
+        background: linear-gradient(
+    270deg,
+    hsla(221, 100%, 86%, 1) 0%,
+    hsla(220, 78%, 29%, 1) 100%
+  );
+  background: -moz-linear-gradient(
+    270deg,
+    hsla(221, 100%, 86%, 1) 0%,
+    hsla(220, 78%, 29%, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    270deg,
+    hsla(221, 100%, 86%, 1) 0%,
+    hsla(220, 78%, 29%, 1) 100%
+  );
     }
     
     * {
